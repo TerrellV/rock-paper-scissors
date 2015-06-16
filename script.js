5130af71bfc82a13254e0aa1971cf6175d7591e1
BigT <terrell.vest@gmail.com>
2015-06-16 16:50:55 -0700

    added display to app

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    alert("i work");
+    alert("Working");
+    
+    //seting variables for result images
+    var rockIcon = "<img src='images/Rock-Icon.png' class='icons'/>";
+    var paperIcon = "<img src='images/Paper-Icon.png' class='icons'/>";
+    var scissorsIcon = "<img src='images/Scissors-Icon.png' class='icons'/>";
+    
+    //set global variables to track user and computer choices
+    var userChoice = "";
+    var setChoice = "";
     
     var compChoice = function() {
         var computerChoice = Math.random();
@@ -14,76 +23,114 @@ $(document).ready(function(){
     }
     
     $("#cRock").click(function() {
-        $(".choices").hide();
-        var userChoice = "rock";
-        var setChoice = compChoice();
+        $(".buttons").hide();
+        userChoice = "rock";
+        setChoice = compChoice();
         runGame(userChoice, setChoice);
-        displayAnswers();
     });
     
     $("#cPaper").click(function() {
-        $(".choices").hide();
-        var userChoice = "paper";
-        var setChoice = compChoice();
+        $(".buttons").hide();
+        userChoice = "paper";
+        setChoice = compChoice();
         runGame(userChoice, setChoice);
     });
     
     $("#cScissors").click(function() {
-        $(".choices").hide();
-        var userChoice = "scissors";
-        var setChoice = compChoice();
+        $(".buttons").hide();
+        userChoice = "scissors";
+        setChoice = compChoice();
         runGame(userChoice, setChoice);
     });
     
+    
     /* Reset Button Click Event*/
     $("#reset").click(function() {
-        var empty = document.getElementById("Answer");
+        var empty = document.getElementById("results");
         empty.innerHTML = "";
-        $(".choices").show(100);
+        $(".buttons").show();
     })
     
     
-    
     var runGame = function(user, comp) {
-
         if (user === comp) {
-            return disChoices(user, comp);
+            disChoices(user, comp);
+            return ;
+            //tie
         };
 
         if (user === "rock") {
             if (comp === "scissors") {
                 disChoices(user, comp);
-                return 10;
+                return;
+                //user wins
             } else {
                 return disChoices(user, comp);
+                // comp wins
             }
         };
 
         if (user === "paper") {
             if (comp === "rock") {
                 return disChoices(user, comp);
+                //user wins
             } else {
                 return disChoices(user, comp);
+                //comp wins
             }
         };
 
         if (user === "scissors") {
             if (comp === "paper") {
-                return disChoices(user, comp);
+                return disChoices("scissors", "paper");
+                //user wins
             } else {
-                return disChoices(user, comp);
+                return disChoices("scissors", "rock");
+                //comp wins
             }
         }
     }
     
+    
     var disChoices = function(user, comp){
-        var elem = document.getElementById("Answer");
-        elem.innerHTML = ("You Chose: " + user +"!" +"<br>" + " The Comp Chose: " + comp+"!");
+        var elem = document.getElementById("results");
         
+        if (user === "rock") {
+            var userImage = rockIcon;    
+        } else if (user === "paper") {
+            var userImage = paperIcon;    
+        } else if (user === "scissors") {
+            var userImage = scissorsIcon;    
+        };
+        
+        if (comp === "rock") {
+            var compImage = rockIcon;    
+        } else if (comp === "paper") {
+            var compImage = paperIcon;    
+        } else if (comp === "scissors") {
+            var compImage = scissorsIcon;    
+        };
+        
+        var inserthtml = "<div id ='b1_2'>" +
+                    "<div id='b1_3'>" +
+                        "<h2 class ='h1s' id='userdis'>USER</h2>" +
+                        "<h2 class ='h1s' id='compdis'>COMP</h2>" +
+                    "</div>" +
+                "</div>" +
+
+                "<div id ='display'>" +
+                    "<div class='ditem'>" + userImage +
+                    "</div>" +
+                    "<div class='ditem'>" +
+                        "<span id='vs'>VS</span>" +
+                    "</div>" +
+                    "<div class='ditem'>" + compImage +
+                    "</div>" +
+                "</div>";
+        
+        elem.innerHTML = inserthtml;
     }
     
-    //var reset = function() {
-    //}
     
   //end program  
 })
