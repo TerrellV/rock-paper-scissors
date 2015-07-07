32435ffc7a500beed39252e802425d42efcac2e0
Terrell V (MirPresT) <terrell.vest@gmail.com>
2015-07-06 22:11:38 -0700

    Switch to angular, add computer algorithm for computer choice

--- script/app.js
+++ script/app.js
@@ -1,13 +1,169 @@
-(function{
-    var App = angular.module('App', []);
+(function(){
+    var myapp = angular.module('myapp', []);
 
-    App.controller = ('HeadingController', function HeadingController($scope){
+    myapp.controller('SetheaderController', function(){
 
-        $scope.author = {
-            'name':'rayBillalobos',
-            'title': 'staf Author',
-            'company': 'Lynda.com'
+        //b represents comp choice - Random || Predicted
+        var b = compChoice();
+        var arr = [];
+
+        // START SCREEN - BEGINNING VIEW
+        this.header = "Choose One";
+        this.showstatsIcon = true;
+        this.showbuttons = true;
+        this.showresults = false;
+        this.showstats = false;
+        this.showbackIcon = false;
+        this.userChoice = "";
+        this.compChoice = "";
+        this.rockIcon = "../images/Rock-Icon.png";
+        this.paperIcon = "../images/Paper-Icon.png";
+        this.scissorsIcon = "../images/Scissors-Icon.png";
+
+///////////////// Game Logic /////////////////////
+
+        // set random value for computer
+        function compChoice() {
+            var computerChoice = Math.random();
+            if (computerChoice < 0.34) {
+                return "R";
+            } else if (computerChoice <= 0.67) {
+                return "P";
+            } else {
+                return "S";
+            }
+        };
+
+        // Decide Winner and Log Stats
+        var runGame = function(user,comp) {
+            if (user === comp) {
+                return logTie();;
+            };
+            if (user === "R") {
+                if (comp === "S") {
+                    return logWin();;
+                } else {
+                    return logLoss();
+                }
+            };
+            if (user === "P") {
+                if (comp === "R") {
+                    return logWin();;
+                } else {
+                    return logLoss();
+                }
+            };
+            if (user === "S") {
+                if (comp === "P") {
+                    return logWin();;
+                } else {
+                    return logLoss();
+                }
+            }
+        };
+
+        // define logging functions
+        function logWin () {
+            UserStats.wins += 1;
+            UserStats.gamesPlayed += 1;
+            return "You Won";
         }
 
-    })
+        function logLoss () {
+            UserStats.lossses += 1;
+            UserStats.gamesPlayed += 1;
+            return "You Lost";
+        }
+
+        function logTie () {
+            UserStats.gamesPlayed += 1;
+            UserStats.ties +=1;
+            return "It's a Tie";
+        }
+
+        // STAT VALUE STORAGE
+        var UserStats = {
+            wins: 0,
+            lossses: 0,
+            ties: 0,
+            gamesPlayed: 0
+        };
+        this.stats = UserStats; // allowing for reference in html
+
+        // Store Given Value Based on Last 2 options.
+        // ex) last 3 choices [R,P,S] set hist.R.P = 'S'
+        var hist = {
+            R:{
+               R:null,
+               P:null,
+               S:null
+            },
+            P:{
+               R:null,
+               P:null,
+               S:null
+            },
+            S:{
+               R:null,
+               P:null,
+               S:null
+            }
+        }
+
+        // return winning options based on predicted choice
+        function botrun () {
+            var next = hist[ arr[1] ][ arr[2] ];
+            if (next === 'R') { return "P";}
+            if (next === 'P') { return "S";}
+            if (next === 'S') { return "R";}
+        };
+
+///////////// Funcitons Dictate major page changes //////////////////////
+
+        // DISPLAY Results when user choice is made
+        this.onOptionPick = function(user) {
+            this.userChoice = user;
+            this.compChoice = b;
+            this.showresults = true;
+            this.showbuttons = false;
+            this.gameResults = runGame(user,b);
+            arr.push(user.slice(0,1).toUpperCase());
+            if (arr.length > 2) {
+                hist[arr[0]][arr[1]] = arr[2];
+            };
+            if (arr.length > 3) {
+                arr.shift();
+                if (botrun() !== undefined) {
+                    b = botrun();
+                }
+            };
+            // function to add classes for animations
+        };
+
+        //STATS SHOW on click of downarrow
+        this.onStatsIconClick = function(){
+            this.header = "User Statistics";
+            this.showbackIcon = true;
+            this.showstats = true;
+            this.showresults = false;
+            this.showbuttons = false;
+            this.showstatsIcon = false;
+            // function to add classes to stats and remove from options
+        }
+
+        // RESET to beginning View on click of back or redCircle
+        this.Reset = function(user,comp){
+            this.header = "Choose One";
+            this.showstatsIcon = true;
+            this.showbuttons = true;
+            this.showbackIcon = false;
+            this.showresults = false;
+            this.showstats = false;
+            // functions to remove classes from stats and results but add to option pannel
+        }
+
+        ////////////////// Add Classes For Animations ///////////////////
+
+
+    });
 })();
--- script/script.js
+++ script/script.js
@@ -2,124 +2,13 @@ $(document).ready(function(){
     $("#backIcon").hide();
     console.log("Working");
     //seting variables for result images
-    var rockIcon = "<img src='../images/Rock-Icon.png' class='icons'/>",
-        paperIcon = "<img src='../images/Paper-Icon.png' class='icons'/>",
-        scissorsIcon = "<img src='../images/Scissors-Icon.png' class='icons'/>";
 
-    //set global variables to track user and computer choices
-    var userImage = "....",
-    compImage = "....",
-    gameResults = ".";
-    // defining computers response
-
-    var compChoice = function() {
-        var computerChoice = Math.random();
-
-        if (computerChoice < 0.34) {
-            return "rock";
-        } else if (computerChoice <= 0.67) {
-            return "paper";
-        } else {
-            return "scissors";
-        }
-    };
-
-    /*setting function and user object to log stats*/
-
-    function logStats(result) {
-        if(result === "win") {
-            UserStats.wins += 1;
-            UserStats.gamesPlayed += 1;
-        } else if (result === "lose") {
-            UserStats.lossses += 1;
-            UserStats.gamesPlayed += 1;
-        } else {
-            UserStats.gamesPlayed += 1;
-            UserStats.ties +=1;
-        }
-    };
-
-    var UserStats = {
-        wins: 0,
-        lossses: 0,
-        ties: 0,
-        gamesPlayed: 0
-    };
-
-    /*seting images on tie function*/
-    var tieImageSet = function(user, comp) {
-        if(user === "rock") {
-        userImage = rockIcon;
-    } else if(user === "paper") {
-        userImage = paperIcon;
-    } else {
-        userImage = scissorsIcon;
-    }
-
-    if(comp === "rock") {
-        compImage = rockIcon;
-    } else if(comp === "paper") {
-        compImage = paperIcon;
-    } else {
-        compImage = scissorsIcon;
-    }};
-
-    // define and set new value for outcome
-
-    var runGame = function(user, comp) {
-        if (user === comp) {
-            tieImageSet(user, comp);
-            logStats("tie");
-            return "Tie Game";
-        };
-        if (user === "rock") {
-            if (comp === "scissors") {
-                compImage = scissorsIcon;
-                userImage = rockIcon;
-                logStats("win");
-                return "You Won";
-            } else {
-                userImage = rockIcon;
-                compImage = paperIcon;
-                logStats("lose");
-                return "You Lost";
-            }
-        };
-        if (user === "paper") {
-            if (comp === "rock") {
-                userImage = paperIcon;
-                compImage =  rockIcon;
-                logStats("win");
-                return "You Won";
-            } else {
-                userImage = paperIcon;
-                compImage = scissorsIcon;
-                logStats("lose");
-                return "You Lost";
-            }
-        };
-        if (user === "scissors") {
-            if (comp === "paper") {
-                userImage = scissorsIcon;
-                compImage = paperIcon;
-                logStats("win");
-                return "You Won";
-            } else {
-                userImage = scissorsIcon;
-                compImage = rockIcon;
-                logStats("lost");
-                return "You Lost";}
-        }
-    };
 
     // DISPLAY STATS STATS STATS STATS STATS
     $('#statsIcon').click(function() { // change element
         $(".buttons").hide();
         $(".statbox").addClass("animated bounce");
         displayStats();
-        var elem = document.getElementById("appHeaderText");
-        elem.innerHTML = "";
-        elem.innerHTML = "<h2>User Statistics</h2>";
         $('#resetButton').removeClass("animated rotateIn")
         $("#backIcon").show();
         $("#statsIcon").hide();
@@ -129,98 +18,37 @@ $(document).ready(function(){
 
     // run the functions above
 
-    $("#cRock").click(function() {
-        $(".buttons").hide();
-        gameResults = runGame("rock", compChoice());
-        displayResults();
-    });
+    // $("#cRock").click(function() {
+    //     $(".buttons").hide();
+    //     gameResults = runGame("rock", compChoice());
+    //     displayResults();
+    // });
+    //
+    // $("#cPaper").click(function() {
+    //     $(".buttons").hide();
+    //     gameResults = runGame("paper", compChoice());
+    //     displayResults();
+    // });
+    //
+    // $("#cScissors").click(function() {
+    //     $(".buttons").hide();
+    //     gameResults = runGame("scissors", compChoice());
+    //     displayResults();
+    // });
 
-    $("#cPaper").click(function() {
-        $(".buttons").hide();
-        gameResults = runGame("paper", compChoice());
-        displayResults();
-    });
-
-    $("#cScissors").click(function() {
-        $(".buttons").hide();
-        gameResults = runGame("scissors", compChoice());
-        displayResults();
-    });
-
-
-    function displayResults (){
-        var elem = document.getElementById("results"),
-            inserthtml =
-                "<div id ='b1_2'>" +
-                    "<div id='b1_3'>" +
-                        "<h2 class ='resultHeader col-lg-6 col-md-6 col-sm-6 col-sm-6' id='userdis'>USER</h2>" +
-                        "<h2 class ='resultHeader col-lg-6 col-md-6 col-sm-6 col-sm-6' id='compdis'>COMP</h2>" +
-                    "</div>" +
-                "</div>" +
-
-                "<div id ='displayImages'>" +
-                    "<div class='displayitem'>" + userImage +
-                    "</div>" +
-                    "<div class='displayitem'>" +
-                        "<span id='vs'>VS</span>" +
-                    "</div>" +
-                    "<div class='displayitem' id='compImage'>" + compImage +
-                    "</div>" +
-                    "<div>" +
-                        "<h3 id ='resultsText'>" + gameResults +
-                        "</h3>" +
-                    "</div>" +
-                "</div>";
-        elem.innerHTML = inserthtml;
-    }
-
-    function displayStats() {
-        var elem = document.getElementById("results"),
-            inserthtml =
-            "<div class='container-fluid statsbox1'>" +
-                "<div class='col-sm-4 col-sm-offset-1 animated fadeIn' id='wins'>" +
-                    "<h3>Wins</h3>" +
-                    "<div class='col-sm-10 col-sm-offset-1 graybox'>" +
-                        "<h4>" + UserStats.wins + "</h4>" +
-                    "</div>"+
-                "</div>" +
-                "<div class='col-sm-4 col-sm-offset-2 animated fadeIn' id='loses'>" +
-                    "<h3>Loses</h3>" +
-                    "<div class='col-sm-10 col-sm-offset-1 graybox'>" +
-                        "<h4>" + UserStats.lossses + "</h4>" +
-                    "</div>" +
-                "</div>" +
-            "</div>" +
-            "<div class='container-fluid statsbox2'>" +
-                "<div class='col-sm-4 col-sm-offset-1 animated fadeIn' id='ties'>" +
-                    "<h3>Ties</h3>" +
-                    "<div class='col-sm-10 col-sm-offset-1 graybox'>" +
-                        "<h4>" + UserStats.ties + "</h4>" +
-                    "</div>" +
-                "</div>" +
-                "<div class='col-sm-4 col-sm-offset-2 animated fadeIn' id='total'>" +
-                    "<h3>Total</h3>" +
-                    "<div class='col-sm-10 col-sm-offset-1 graybox'>" +
-                        "<h4>" + UserStats.gamesPlayed +"</h4>" +
-                    "</div>" +
-                "</div>" +
-            "</div>";
-        elem.innerHTML = inserthtml;
-    }
-
-    $(".buttons").click(function() {
-        $('#resultsText').hide();
-        $("#resetButton").removeClass("animated rotateIn");
-        $(".buttons").removeClass("animated fadeIn");
-        $("#compImage").addClass("animated bounceInDown");
-        $('#vs').addClass("animated zoomIn");
-
-        if (gameResults === "You Won"){
-            $('#resultsText').delay(2000).show(0).addClass("animated flash");
-        } else {
-            $('#resultsText').delay(2000).show(0).addClass("animated shake");
-        }
-    });
+    // $(".buttons").click(function() {
+    //     $('#resultsText').hide();
+    //     $("#resetButton").removeClass("animated rotateIn");
+    //     $(".buttons").removeClass("animated fadeIn");
+    //     $("#compImage").addClass("animated bounceInDown");
+    //     $('#vs').addClass("animated zoomIn");
+    //
+    //     if (gameResults === "You Won"){
+    //         $('#resultsText').delay(2000).show(0).addClass("animated flash");
+    //     } else {
+    //         $('#resultsText').delay(2000).show(0).addClass("animated shake");
+    //     }
+    // });
 
     // backbutton click event
     $("#backIcon").click(function() {
@@ -230,7 +58,6 @@ $(document).ready(function(){
         $(".buttons").addClass("animated fadeIn");
         $("#compImage").removeClass("animated bounceInDown");
         $("#vs").removeClass("animated zoomIn");
-        ResetAppTextHeader();
         $("#backIcon").hide();
         $("#statsIcon").show();
     });
@@ -251,16 +78,9 @@ $(document).ready(function(){
         $(".buttons").addClass("animated fadeIn");
         $("#compImage").removeClass("animated bounceInDown");
         $("#vs").removeClass("animated zoomIn");
-        ResetAppTextHeader();
         $("#backIcon").hide();
         $("#statsIcon").show();
     });
-
-    function ResetAppTextHeader() {
-        var elem = document.getElementById("appHeaderText");
-        elem.innerHTML = "";
-        elem.innerHTML = "<h2>Choose One</h2>";
-    }
 
     function clearresults() {
         var emptyresults = document.getElementById("results");
